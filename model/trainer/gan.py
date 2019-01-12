7bbd63362425d5c107818e2378a84c22afee2176
Adam Snaider <adsnaider@gmail.com>
2019-01-12 15:26:33 -0800

    Started working on ml engine.

--- model/trainer/gan.py
+++ model/trainer/gan.py
@@ -4,16 +4,18 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+# import tensorflow.contrib.eager as tfe
 from absl import app
 from absl import flags
 from absl import logging
-from tensorflow.contrib import eager as tfe
 
-import audio_models
+from trainer import audio_models
 
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 
 FLAGS = flags.FLAGS
+
+flags.DEFINE_string('job-dir', None, "Job directory")
 
 flags.DEFINE_string('genre1', None,
                     'tfrecords file with all the songs from genre 1')
@@ -62,22 +64,32 @@ class GAN():
 def main(argv):
   del argv
 
+  def _parse_func(example_proto):
+    features = {
+        'song': tf.FixedLenFeature((), tf.string, default_value=""),
+        'sample_rate': tf.FixedLenFeature((), tf.int64, default_value=0)
+    }
+
+    parsed_features = tf.parse_single_example(example_proto, features)
+    return parsed_features["song"], parsed_features["sample_rate"]
+
   genre1 = tf.data.TFRecordDataset(FLAGS.genre1)
+  genre1 = genre1.map(_parse_func)
   genre1 = genre1.shuffle(buffer_size=100)
   genre1 = genre1.batch(FLAGS.batch_size)
   genre1 = genre1.repeat()
+  # genre1 = tfe.Iterator(genre1)
+  genre1 = genre1.make_one_shot_iterator()
+  genre1_songs = genre1.get_next()
 
   genre2 = tf.data.TFRecordDataset(FLAGS.genre2)
+  genre2 = genre2.map(_parse_func)
   genre2 = genre2.shuffle(buffer_size=100)
   genre2 = genre2.batch(FLAGS.batch_size)
   genre2 = genre2.repeat()
-
-  genre1 = tfe.Iterator(
-      tf.data.Dataset.from_tensor_slices(
-          np.random.normal(size=[100, 6000, 1])).batch(1))
-  genre2 = tfe.Iterator(
-      tf.data.Dataset.from_tensor_slices(
-          np.random.normal(size=[100, 6000, 1])).batch(1))
+  # genre2 = tfe.Iterator(genre2)
+  genre2 = genre2.make_one_shot_iterator()
+  genre2_songs = genre2.get_next()
 
   g1 = GAN(audio_models.AudioEncoderDecoder(), audio_models.AudioClassifier())
   g2 = GAN(audio_models.AudioEncoderDecoder(), audio_models.AudioClassifier())
@@ -87,47 +99,37 @@ def main(argv):
 
   global_step = tf.train.get_or_create_global_step()
 
-  for i in range(FLAGS.train_steps):
-    global_step.assign_add(1)
-    genre1_songs = next(genre1)
-    genre2_songs = next(genre2)
+  global_step_update = global_step.assign_add(1)
 
-    with tf.GradientTape(persistent=True) as tape:
-      d_loss1, g_loss1, generated_genre2 = g1.loss(genre1_songs, genre2_songs)
-      d_loss2, g_loss2, generated_genre1 = g2.loss(genre2_songs, genre1_songs)
+  d_loss1, g_loss1, generated_genre2 = g1.loss(genre1_songs, genre2_songs)
+  d_loss2, g_loss2, generated_genre1 = g2.loss(genre2_songs, genre1_songs)
 
-      cycle1 = g2.generate(generated_genre2)
-      cycle_loss1 = tf.cast(
-          tf.losses.mean_squared_error(cycle1, genre1_songs), dtype=tf.float64)
+  cycle1 = g2.generate(generated_genre2)
+  cycle_loss1 = tf.cast(
+      tf.losses.mean_squared_error(cycle1, genre1_songs), dtype=tf.float64)
 
-      cycle2 = g1.generate(generated_genre1)
-      cycle_loss2 = tf.cast(
-          tf.losses.mean_squared_error(cycle2, genre2_songs), dtype=tf.float64)
+  cycle2 = g1.generate(generated_genre1)
+  cycle_loss2 = tf.cast(
+      tf.losses.mean_squared_error(cycle2, genre2_songs), dtype=tf.float64)
 
-      g_loss1 = g_loss1 + FLAGS.cycle_weight * cycle_loss1
-      g_loss2 = g_loss2 + FLAGS.cycle_weight * cycle_loss2
+  g_loss1 = g_loss1 + FLAGS.cycle_weight * cycle_loss1
+  g_loss2 = g_loss2 + FLAGS.cycle_weight * cycle_loss2
 
-    g_opt.apply_gradients(
-        zip(
-            tape.gradient(g_loss1, g1.gen.trainable_variables),
-            g1.gen.trainable_variables))
-    g_opt.apply_gradients(
-        zip(
-            tape.gradient(g_loss2, g2.gen.trainable_variables),
-            g2.gen.trainable_variables))
-    d_opt.apply_gradients(
-        zip(
-            tape.gradient(d_loss1, g1.disc.trainable_variables),
-            g1.disc.trainable_variables))
-    d_opt.apply_gradients(
-        zip(
-            tape.gradient(d_loss2, g2.disc.trainable_variables),
-            g2.disc.trainable_variables))
+  train_op = tf.group([
+      g_opt.minimize(g_loss1, var_list=g1.gen.trainable_variables),
+      g_opt.minimize(g_loss2, var_list=g2.gen.trainable_variables),
+      d_opt.minimize(d_loss1, var_list=g1.disc.trainable_variables),
+      d_opt.apply_gradients(d_loss2, g2.disc.trainable_variables)
+  ])
 
-    del tape
+  with tf.train.MonitoredTrainingSession() as sess:
 
-    print("Step {} : D loss : {} : G loss {}".format(
-        i, (d_loss1 + d_loss2).numpy(), (g_loss1 + g_loss2).numpy()))
+    for _ in range(FLAGS.train_steps):
+      _, DLoss1, GLoss1, DLoss2, GLoss2 = sess.run(
+          [train_op, d_loss1, g_loss1, d_loss2, g_loss2])
+      sess.run(global_step_update)
+      print("Step {} : D loss : {} : G loss {}".format(
+          global_step.eval(), DLoss1 + DLoss2, GLoss1 + GLoss2))
 
 
 if __name__ == '__main__':
