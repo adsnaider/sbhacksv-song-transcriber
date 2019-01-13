a9f8e2c81566e6ef72804bd6c500594e0436241d
Adam Snaider <adsnaider@gmail.com>
2019-01-12 20:17:01 -0800

    Changed genres.

--- model/run_cloud.sh
+++ model/run_cloud.sh
@@ -1,4 +1,5 @@
 gcloud ml-engine jobs submit training $1    --runtime-version 1.12 \
 --config config.yaml   --module-name trainer.gan           --staging-bucket gs://song-repo \
 --package-path trainer/  -- --job_dir "gs://song-repo/training/$1/" \
---genre1 gs://song-repo/fma_small/tfrecords/10.tfrecords --genre2 gs://song-repo/fma_small/tfrecords/2.tfrecords
+--genre1 gs://song-repo/fma_small/tfrecords/15.tfrecords \
+--genre2 gs://song-repo/fma_small/tfrecords/17.tfrecords
